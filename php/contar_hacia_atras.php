cea0c7af8ce2b14654b40e64ed45e3bc875295ce
baquerovaliente23 <baquerovaliente23@iespabloserrano.com>
2024-10-31 18:32:17 +0100

    todo ordenado

--- php/contar_hacia_atras.php
+++
@@ -1,9 +0,0 @@
-<?php
-$n1 = 10; // Cambia este valor según lo necesites
-if ($n1 < 0) {
-    $n1 = -$n1;
-}
-for ($i = $n1; $i >= 0; $i--) {
-    echo $i . " ";
-}
-?>
--- php/lanzamientos_dado.php
+++
@@ -1,8 +0,0 @@
-<?php
-$suma = 0;
-for ($i = 1; $i <= 10; $i++) {
-    $dado = rand(1, 6);
-    $suma += $dado;
-    echo "Lanzamiento $i: $dado, Suma acumulada: $suma <br>";
-}
-?>
